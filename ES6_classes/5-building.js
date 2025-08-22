c9e38c3db45ccdd40e88d69e09f32a17f37f789c
Khulood AlOtaibi <52895786+KhuloodGhazi@users.noreply.github.com>
2025-08-22 15:43:59 +0300

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,29 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building
-      && this.evacuationWarningMessage === undefined) {
-      throw Error('Class extending Building must override evacuationWarningMessage');
+    // Prevent direct instantiation of Building
+    if (new.target === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly');
     }
+
     this._sqft = sqft;
+
+    // Ensure subclass overrides evacuationWarningMessage
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
+  // Getter and setter for sqft
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // Abstract method
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
